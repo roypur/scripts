3f1292d41f5977f62a616de25bf317bd3683a3fa
Roy Olav Purser <admin@royolav.net>
2017-10-10 11:52:05 +0200

    fix installer

--- keyboard/install.sh
+++ keyboard/install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-sudo bash --command << EOF
-wget -O /usr/share/X11/xkb/rules/evdev.xml 
-wget -O /usr/share/X11/xkb/symbols/roypur-no
-EOF
+folder=$(mktemp --directory)
+mkdir ${folder}/rules
+mkdir ${folder}/symbols
+
+curl --silent --output ${folder}/rules/evdev.xml "https://raw.githubusercontent.com/roypur/scripts/master/keyboard/evdev.xml"
+curl --silent --output ${folder}/symbols/roypur-no "https://raw.githubusercontent.com/roypur/scripts/master/keyboard/roypur-no"
+sudo cp --recursive ${folder}/* /usr/share/X11/xkb
+
+rm -rf ${folder}
+
+echo "Custom keyboard layout installed."
