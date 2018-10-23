4485853d651959bc67a15facf4dab9db09f05539
Roy Olav Purser <admin@royolav.net>
2018-10-23 12:54:00 +0100

    add gb layout

--- keyboard/install.sh
+++ keyboard/install.sh
@@ -5,6 +5,7 @@ mkdir ${folder}/symbols
 
 curl --silent --output ${folder}/rules/evdev.xml "https://raw.githubusercontent.com/roypur/scripts/master/keyboard/evdev.xml"
 curl --silent --output ${folder}/symbols/roypur-no "https://raw.githubusercontent.com/roypur/scripts/master/keyboard/roypur-no"
+curl --silent --output ${folder}/symbols/roypur-gb "https://raw.githubusercontent.com/roypur/scripts/master/keyboard/roypur-gb"
 sudo cp --recursive ${folder}/* /usr/share/X11/xkb
 
 rm -rf ${folder}
