5389533267493089fa905764b657993f8fd6c7d9
Roy Olav Purser <admin@royolav.net>
2019-02-06 14:17:20 +0000

    update keyboard layout for wayland

--- keyboard/install.sh
+++ keyboard/install.sh
@@ -4,7 +4,8 @@ mkdir ${folder}/rules
 mkdir ${folder}/symbols
 
 curl --silent --output ${folder}/rules/evdev.xml "https://raw.githubusercontent.com/roypur/scripts/master/keyboard/evdev.xml"
-curl --silent --output ${folder}/symbols/roypur-no "https://raw.githubusercontent.com/roypur/scripts/master/keyboard/roypur-no"
+curl --silent --output ${folder}/rules/evdev.lst "https://raw.githubusercontent.com/roypur/scripts/master/keyboard/evdev.lst"
+curl --silent --output ${folder}/symbols/roypur-no "https://raw.githubusercontent.com/roypur/scripts/master/keyboard/roypur-gb"
 curl --silent --output ${folder}/symbols/roypur-gb "https://raw.githubusercontent.com/roypur/scripts/master/keyboard/roypur-gb"
 sudo cp --recursive ${folder}/* /usr/share/X11/xkb
 
