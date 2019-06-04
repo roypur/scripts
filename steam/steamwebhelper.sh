b76e515fdec981d7e421b78db0c8c76b70b1ca60
Roy Olav Purser <admin@royolav.net>
2019-06-04 14:36:03 +0100

    fix steamwebhelper

--- steam/steamwebhelper.sh
+++ steam/steamwebhelper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 dir=$(dirname $(realpath $0))
-export LD_LIBRARY_PATH="/usr/lib64:/usr/lib32:/usr/lib64/libva1:/usr/lib32/libva1:${dir}"
+export LD_LIBRARY_PATH="/usr/lib64:/usr/lib32:/usr/lib64/libva1:/usr/lib32/libva1:${LD_LIBRARY_PATH}:${dir}"
 cd "${dir}"
 
 ./steamwebhelper "$@"
