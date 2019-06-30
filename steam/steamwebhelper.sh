79d1a1c2473abb1905982058f0bd613f4fba8f7e
Roy Olav Purser <admin@royolav.net>
2019-06-30 20:38:45 +0200

    Delete steamwebhelper.sh

--- steam/steamwebhelper.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-dir=$(dirname $(realpath $0))
-export LD_LIBRARY_PATH="/usr/lib64:/usr/lib32:/usr/lib64/libva1:/usr/lib32/libva1:${LD_LIBRARY_PATH}:${dir}"
-cd "${dir}"
-
-./steamwebhelper "$@"
