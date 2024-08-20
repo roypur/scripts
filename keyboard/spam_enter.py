9d23a086874fa8a81c0d466163088d2931a8fa27
Roy Olav Purser <admin@royolav.net>
2024-08-20 22:47:22 +0200

    Retry binding controller

--- keyboard/spam_enter.py
+++ keyboard/spam_enter.py
@@ -4,23 +4,36 @@ import evdev
 
 KEYBOARD_NAME = "8BitDo"
 
+
 def get_keyboard() -> str:
     for device_name in evdev.list_devices():
-        if KEYBOARD_NAME.lower() in (device := evdev.InputDevice(device_name)).name.lower():
+        if (
+            KEYBOARD_NAME.lower()
+            in (device := evdev.InputDevice(device_name)).name.lower()
+        ):
             return device
     raise KeyError
 
 
-device = get_keyboard()
-device.grab()
-print(device)
+def loop() -> None:
+    device = get_keyboard()
+    device.grab()
+    print(device)
 
-uinput = evdev.uinput.UInput()
+    uinput = evdev.uinput.UInput()
+    while True:
+        time.sleep(0.05)
+        for key in device.active_keys():
+            if key == evdev.ecodes.BTN_EAST:
+                uinput.write(evdev.ecodes.EV_KEY, evdev.ecodes.KEY_ENTER, 1)
+                uinput.syn()
+                uinput.write(evdev.ecodes.EV_KEY, evdev.ecodes.KEY_ENTER, 0)
+                uinput.syn()
+
+
 while True:
-    time.sleep(0.05)
-    for key in device.active_keys():
-        if key == evdev.ecodes.BTN_EAST:
-            uinput.write(evdev.ecodes.EV_KEY, evdev.ecodes.KEY_ENTER, 1)
-            uinput.syn()
-            uinput.write(evdev.ecodes.EV_KEY, evdev.ecodes.KEY_ENTER, 0)
-            uinput.syn()
+    try:
+        loop()
+    except (SystemError, KeyError):
+        print("8BitDo controller not connected. Retrying")
+        time.sleep(1)
