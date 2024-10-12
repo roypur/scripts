ec354d26acb6605abe9dadf0ab47da06f2314e1e
Roy Olav Purser <admin@royolav.net>
2024-10-12 19:03:22 +0200

    Add tampermonkey script for moodle

---
+++ tampermonkey/zoom-link-replace.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Direct link to zoom meeting
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Direct link to zoom meeting
+// @author       roypur
+// @match        https://moodle.hsky.fi/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=hsky.fi
+// @updateURL    https://github.com/roypur/scripts/raw/main/tampermonkey/zoom-link-replace.user.js
+// @downloadURL  https://github.com/roypur/scripts/raw/main/tampermonkey/zoom-link-replace.user.js
+// @grant        none
+// ==/UserScript==
+
+(() => {
+  const links = document.getElementsByTagName("a");
+  for (const link of links) {
+    const url = new URL(link.href);
+    if (url.hostname.includes("zoom.us")) {
+      const splitted = url.pathname.split("/");
+      let meetingId = null;
+      for (const part of splitted) {
+        if (part) {
+          meetingId = part;
+        }
+      }
+      url.pathname = `/wc/join/${meetingId}`;
+      link.href = url.href;
+      link.innerText = url.href;
+    }
+  }
+})();
