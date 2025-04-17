6040ff79138dd7130ed166678fb4ff5e75c1d71e
Roy Olav Purser <admin@royolav.net>
2025-04-17 13:12:57 +0200

    Add mouse press

--- keyboard/spam_enter.py
+++ keyboard/spam_enter.py
@@ -4,6 +4,19 @@ import evdev
 
 KEYBOARD_NAME = "8BitDo"
 
+CAPABILITIES = {
+    evdev.ecodes.EV_KEY: [
+        evdev.ecodes.KEY_ENTER,
+        evdev.ecodes.BTN_LEFT,
+    ],
+    evdev.ecodes.EV_REL: [
+        evdev.ecodes.REL_X,
+        evdev.ecodes.REL_Y,  # Cursor movement
+        evdev.ecodes.REL_WHEEL,
+        evdev.ecodes.REL_HWHEEL,  # Scroll wheel (optional)
+    ],
+}
+
 
 def get_keyboard() -> str:
     for device_name in evdev.list_devices():
@@ -20,14 +33,19 @@ def loop() -> None:
     device.grab()
     print(device)
 
-    uinput = evdev.uinput.UInput()
+    uinput = evdev.uinput.UInput(CAPABILITIES)
     while True:
         time.sleep(0.05)
         for key in device.active_keys():
-            if key == evdev.ecodes.BTN_EAST:
+            if key == evdev.ecodes.BTN_B:
                 uinput.write(evdev.ecodes.EV_KEY, evdev.ecodes.KEY_ENTER, 1)
                 uinput.syn()
                 uinput.write(evdev.ecodes.EV_KEY, evdev.ecodes.KEY_ENTER, 0)
+                uinput.syn()
+            if key == evdev.ecodes.BTN_Y:
+                uinput.write(evdev.ecodes.EV_KEY, evdev.ecodes.BTN_MOUSE, 1)
+                uinput.syn()
+                uinput.write(evdev.ecodes.EV_KEY, evdev.ecodes.BTN_MOUSE, 0)
                 uinput.syn()
 
 
