b337dedae1274ae74c27b4eef4824360d5b983ce
Roy Olav Purser <admin@royolav.net>
2026-05-22 23:40:15 +0300

    Add network status to sway

--- sway/sway-status.py
+++ sway/sway-status.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from typing import Literal
 import subprocess
 import os
 import json
@@ -8,6 +9,7 @@ from typing import Final
 from pathlib import Path
 import socket
 import asyncio
+import dataclasses
 
 SOCKET_PATH: Final[str] = str(Path.home() / ".config/sway-status-vpn.sock")
 VPN_STATUS: str = ""
@@ -42,6 +44,19 @@ class ConfigFile(pydantic.BaseModel):
     battery_status_file: str = "/sys/class/power_supply/BAT0/status"
 
 
+@dataclasses.dataclass
+class NetworkStatus:
+    network_type: Literal["wifi", "wired"]
+    device: str
+    ssid: str
+
+
+@dataclasses.dataclass
+class DefaultRoute:
+    ipv4: NetworkStatus | None
+    ipv6: NetworkStatus | None
+
+
 def get_config() -> ConfigFile:
     try:
         with open(
@@ -72,15 +87,103 @@ def get_layout() -> str:
             if layout := device.get("xkb_active_layout_name"):
                 layout = layout.lower()
                 if "english" in layout:
-                    return "uk"
+                    return "uk | "
                 elif "norwegian" in layout:
-                    return "no"
+                    return "no | "
                 elif "finnish" in layout:
-                    return "fi"
+                    return "fi | "
                 return layout
     except Exception:
         pass
-    return "layout not found"
+    return ""
+
+
+def get_network_status() -> list[NetworkStatus]:
+    try:
+        result = subprocess.run(
+            [
+                "nmcli",
+                "--terse",
+                "--fields",
+                "type,device,name",
+                "connection",
+                "show",
+                "--active",
+            ],
+            check=True,
+            capture_output=True,
+            encoding="utf-8",
+        )
+        connections: list[NetworkStatus] = []
+        for line in result.stdout.splitlines():
+            splitted = line.strip().split(":")
+            if len(splitted) == 3:
+                if splitted[0] == "802-11-wireless":
+                    connections.append(
+                        NetworkStatus(
+                            network_type="wifi",
+                            device=splitted[1],
+                            ssid=splitted[2],
+                        )
+                    )
+                elif splitted[0] == "802-3-ethernet":
+                    connections.append(
+                        NetworkStatus(
+                            network_type="wired",
+                            device=splitted[1],
+                            ssid="",
+                        )
+                    )
+        return connections
+    except Exception:
+        pass
+    return []
+
+
+def get_default_route(connections: list[NetworkStatus]) -> DefaultRoute:
+    routes = DefaultRoute(ipv4=None, ipv6=None)
+
+    def route_ipv4() -> None:
+        try:
+            nonlocal routes
+            result = subprocess.run(
+                ["ip", "-4", "route"],
+                check=True,
+                capture_output=True,
+                encoding="utf-8",
+            )
+            for line in result.stdout.splitlines():
+                splitted = line.split()
+                if splitted[0] == "default":
+                    for connection in connections:
+                        if splitted[4] == connection.device:
+                            routes.ipv4 = connection
+                            return
+        except Exception:
+            pass
+
+    def route_ipv6() -> None:
+        try:
+            nonlocal routes
+            result = subprocess.run(
+                ["ip", "-6", "route"],
+                check=True,
+                capture_output=True,
+                encoding="utf-8",
+            )
+            for line in result.stdout.splitlines():
+                splitted = line.split()
+                if splitted[0] == "default":
+                    for connection in connections:
+                        if splitted[4] == connection.device:
+                            routes.ipv6 = connection
+                            return
+        except Exception:
+            pass
+
+    route_ipv4()
+    route_ipv6()
+    return routes
 
 
 def read_battery() -> int:
@@ -107,10 +210,35 @@ def read_battery_status() -> int:
     return 0
 
 
+def network_status_entry() -> str:
+    ipv4 = ""
+    ipv6 = ""
+    default_route = get_default_route(get_network_status())
+    if default_route.ipv4 is not None:
+        if default_route.ipv4.network_type == "wifi":
+            ipv4 = f"v4[{default_route.ipv4.network_type},{default_route.ipv4.ssid}]"
+        else:
+            ipv4 = f"v4[{default_route.ipv4.network_type}]"
+    if default_route.ipv6 is not None:
+        if default_route.ipv6.network_type == "wifi":
+            ipv6 = f"v6[{default_route.ipv6.network_type},{default_route.ipv6.ssid}]"
+        else:
+            ipv6 = f"v6[{default_route.ipv6.network_type}]"
+    if ipv4 and ipv6:
+        return f"{ipv4} | {ipv6} | "
+    if ipv4:
+        return f"{ipv4} | "
+    if ipv6:
+        return f"{ipv6} | "
+    return ""
+
+
 async def start_loop() -> None:
     last_line = ""
     while True:
         await asyncio.sleep(0.1)
+        network_status = network_status_entry()
+
         layout = get_layout()
         battery = read_battery()
         battery_status = read_battery_status()
@@ -126,17 +254,19 @@ async def start_loop() -> None:
             tzinfo=ctime.tzinfo,
         ).isoformat()
 
-        vpn_prefix: str = f"{VPN_STATUS} "
-        if not vpn_prefix.strip():
-            vpn_prefix = ""
+        vpn_prefix: str = ""
+        if trimmed_vpn_status := VPN_STATUS.strip():
+            vpn_prefix = f"{trimmed_vpn_status} | "
 
-        next_line = f"{vpn_prefix}{layout} {pretty_time}"
-        if battery and battery_status:
+        battery_entry = ""
+        if battery:
+            battery_entry = f"{battery}% | "
             if battery_status == 1:
-                next_line = f"{vpn_prefix}{layout} +{battery}% {pretty_time}"
-            else:
-                next_line = f"{vpn_prefix}{layout} {battery}% {pretty_time}"
+                battery_entry = f"+{battery}% | "
 
+        next_line = (
+            f"{network_status}{vpn_prefix}{layout}{battery_entry}{pretty_time} | "
+        )
         if last_line != next_line:
             last_line = next_line
             print(next_line, flush=True)
